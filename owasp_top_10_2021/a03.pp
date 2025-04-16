a1922d56764dbc600575b19ca7fc9575ad91eae2
Cody Bruno <cody@turbot.com>
2025-04-16 00:59:42 -0400

    Sort detections

--- owasp_top_10_2021/a03.pp
+++ owasp_top_10_2021/a03.pp
@@ -10,15 +10,6 @@ benchmark "owasp_top_10_2021_a03" {
   type          = "detection"
   documentation = file("./owasp_top_10_2021/docs/a01.md")
   children = [
-    # SQL Injection detections
-    detection.sql_injection_blind_based,
-    detection.sql_injection_common_patterns,
-    detection.sql_injection_error_based,
-    detection.sql_injection_time_based,
-    detection.sql_injection_union_based,
-    detection.sql_injection_user_agent_based,
-
-    # Cross-Site Scripting detections (explicitly tagged with A03)
     detection.cross_site_scripting_angular_template,
     detection.cross_site_scripting_attribute_injection,
     detection.cross_site_scripting_common_patterns,
@@ -28,10 +19,14 @@ benchmark "owasp_top_10_2021_a03" {
     detection.cross_site_scripting_javascript_methods,
     detection.cross_site_scripting_javascript_uri,
     detection.cross_site_scripting_script_tag,
-
-    # Remote Command Execution detections
     detection.log4shell_vulnerability,
     detection.spring4shell_vulnerability
+    detection.sql_injection_blind_based,
+    detection.sql_injection_common_patterns,
+    detection.sql_injection_error_based,
+    detection.sql_injection_time_based,
+    detection.sql_injection_union_based,
+    detection.sql_injection_user_agent_based,
   ]
 
   tags = merge(local.owasp_top_10_2021_a03_common_tags, {
